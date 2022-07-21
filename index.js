2a1d28fe71164ad370d3ed95e672f0028f5cb079
husseinalaa15 <hussein.alaa12199@gmail.com>
2022-07-22 01:56:38 +0200

    env

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const http = require('http');
 const mongoose = require('mongoose');
 
 const app = require('./backend/app');
+require("dotenv").config();
+
 
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || 'localhost';
+
+
 
 app.set('PORT',PORT)
 const server = http.createServer(app)
@@ -13,7 +18,7 @@ const server = http.createServer(app)
 mongoose.connect('mongodb+srv://EBookalypse:rWCaMtf7016NIOPs@cluster0.jgx2q.mongodb.net/BookStore?retryWrites=true&w=majority')
 .then(()=>{
     server.listen(PORT,()=>{
-        console.log(`we are listening to 'http://localhost:${PORT}/'`)
+        console.log(`we are listening to ${HOST}:${PORT}/'`)
     })
 })
 .catch((err)=>{console.log(err)})
