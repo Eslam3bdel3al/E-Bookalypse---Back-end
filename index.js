34774c7808211f94e80f9f261ca3b50e978896f6
husseinalaa15 <hussein.alaa12199@gmail.com>
2022-07-25 01:37:59 +0200

    merging Work 25-7 (api routes naming)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = require('./backend/app');
 require("dotenv").config();
 
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 const host = process.env.HOST || 'localhost';
 const DATABASE = process.env.DATABASE || 'mongodb+srv://EBookalypse:rWCaMtf7016NIOPs@cluster0.jgx2q.mongodb.net/BookStore?retryWrites=true&w=majority'
 
