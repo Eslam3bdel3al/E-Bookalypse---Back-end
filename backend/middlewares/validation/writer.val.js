aab8983734c434b58f307b7f94262096cd4b7042
Eslam3bdel3al <abdelalleslam@gmail.com>
2022-08-18 01:18:57 +0200

    optional in express validation

--- backend/middlewares/validation/writer.val.js
+++ backend/middlewares/validation/writer.val.js
@@ -3,19 +3,19 @@ const {body,param} = require("express-validator")
 
 module.exports.writerAdd = [
     body("name").isAlpha().withMessage("should be Letters only").notEmpty().withMessage("This Field is required"),
-    body("gender").isAlpha().withMessage("gender can contain only letters").isIn(["male","female"]),
-    body("bio").isString().withMessage("bio must be a string"),
-    body("date_birth").isDate().withMessage("Birth date is not valid"),
-    body("place_birth").isString().withMessage("governorate must be a string")
+    body("gender").optional().isAlpha().withMessage("gender can contain only letters").isIn(["male","female"]),
+    body("bio").optional().isString().withMessage("bio must be a string"),
+    body("date_birth").optional().isDate().withMessage("Birth date is not valid"),
+    body("place_birth").optional().isString().withMessage("governorate must be a string")
 ]
 
 module.exports.writerUpdate = [
     param("writerId").isMongoId().withMessage("param must be a mongo id").notEmpty().withMessage("writerId is required"),
     body("name").isAlpha().withMessage("should be Letters only").notEmpty().withMessage("This Field is required"),
-    body("gender").isAlpha().withMessage("gender can contain only letters").isIn(["male","female"]),
-    body("bio").isString().withMessage("bio must be a string"),
-    body("date_birth").isDate().withMessage("Birth date is not valid"),
-    body("place_birth").isString().withMessage("governorate must be a string")
+    body("gender").optional().isAlpha().withMessage("gender can contain only letters").isIn(["male","female"]),
+    body("bio").optional().isString().withMessage("bio must be a string"),
+    body("date_birth").optional().isDate().withMessage("Birth date is not valid"),
+    body("place_birth").optional().isString().withMessage("governorate must be a string")
 ]
 
 module.exports.writerParam = [
