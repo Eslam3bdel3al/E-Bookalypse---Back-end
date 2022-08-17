32453128d0024977fbabbb3350dd7f272027a396
Eslam3bdel3al <abdelalleslam@gmail.com>
2022-08-17 23:51:28 +0200

    cat valistion fixation

--- backend/middlewares/validation/category.val.js
+++ backend/middlewares/validation/category.val.js
@@ -1,12 +1,12 @@
 const {body,param} = require("express-validator")
 
 module.exports.categoryAdd = [
-    body("title").isString().withMessage("should be Letters only").notEmpty().withMessage("This Field is required"),
+    body("catTitle").isString().withMessage("should be Letters only").notEmpty().withMessage("catTitle is required"),
 ]
 
 module.exports.categoryEdit = [
     param("catId").isMongoId().withMessage("param must be a mongo id").notEmpty().withMessage("catId is required"),
-    body("title").isString().withMessage("should be Letters only").notEmpty().withMessage("This Field is required")
+    body("catTitle").isString().withMessage("should be Letters only").notEmpty().withMessage("catTitle is required")
 ]
 
 module.exports.catParam = [
