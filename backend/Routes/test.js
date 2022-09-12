da0ecf61536d4dc75afd1a30e16d5e0be9feb6ae
Eslam3bdel3al <abdelalleslam@gmail.com>
2022-09-12 15:09:12 +0200

    first commit to my repo

--- backend/Routes/test.js
+++
@@ -1,9 +0,0 @@
-string = "2022-08-27T22:00:00.000Z";
-
-let year = string.split("T")[0].split("-")[0];
-
-let month = string.split("T")[0].split("-")[1];
-
-let day = string.split("T")[0].split("-")[2];
-
-const endDate = new Date(`${month}/${month}/${year}`)
